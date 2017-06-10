0c138d7ab557abf7112e79f0f1a3aaf7d928723d
Ridvan Baluyos <ridvan@baluyos.net>
2017-06-10 17:34:12 +0800

    initial commit

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Example Component</div>
+
+                    <div class="panel-body">
+                        I'm an example component!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    console.log('test');
+    export default {
+        ready() {
+            console.log('Component ready.')
+
+            this.$http.get('/api/user')
+                .then(response => {
+                    console.log(response.data);
+                });
+        }
+    }
+</script>
